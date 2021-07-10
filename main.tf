856df98b522eac403bbb83faf1dd3e0517d8e6c0
Jacob LeBlanc <leblancthomasjacob@gmail.com>
2021-07-10 16:33:46 -0300

    Added subnets from cloud posse module

--- main.tf
+++ main.tf
@@ -11,3 +11,14 @@ module "vpc" {
   name       = "main-vpc"
   cidr_block = "10.0.0.0/16"
 }
+
+module "subnets" {
+  source     = "cloudposse/dynamic-subnets/aws"
+  version    = "0.39.3"
+  namespace  = var.namespace
+  stage      = var.stage
+  name       = "main-subnet"
+  vpc_id     = module.vpc.vpc_id
+  igw_id     = module.vpc.igw_id
+  cidr_block = "10.0.0.0/16"
+}
