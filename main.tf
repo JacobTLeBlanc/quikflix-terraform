15c7566a32e9652bb65f1912e83ef6b4db88f079
Jacob LeBlanc <leblancthomasjacob@gmail.com>
2021-07-10 16:25:38 -0300

    Changed VPC to use cloud posse module

--- main.tf
+++ main.tf
@@ -3,6 +3,11 @@ provider "aws" {
 }
 
 # Main VPC
-resource "aws_vpc" "example" {
+module "vpc" {
+  source     = "cloudposse/vpc/aws"
+  version    = "0.25.0"
+  namespace  = var.namespace
+  stage      = var.stage
+  name       = "main-vpc"
   cidr_block = "10.0.0.0/16"
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,21 @@ variable "region" {
   type = string
   default = "us-east-2"
 }
+
+variable "availability_zones" {
+  type = list(string)
+  default = ["us-east-2a", "us-east-2b", "us-east-2c"]
+}
+
+variable "namespace" {
+  type = string
+}
+
+variable "stage" {
+  type = string
+}
+
+# Movie Catalog variables
+variable "movie_catalog_name" {
+  type = string
+}
