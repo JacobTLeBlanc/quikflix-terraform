975e526ea9a3470f5196eb68a8afaec6a661c14e
Jacob LeBlanc <leblancthomasjacob@gmail.com>
2021-07-11 14:56:00 -0300

    Added Beanstalk environment for movie catalog

--- movie-catalog/movie-catalog.tf
+++ movie-catalog/movie-catalog.tf
@@ -1,6 +1,6 @@
 module "elastic_beanstalk_application" {
   source      = "cloudposse/elastic-beanstalk-application/aws"
-  version     = "0.40.0"
+  version     = "0.11.0"
   namespace   = var.namespace
   stage       = var.stage
   name        = var.name
